798ee8520d210c938555d9287d2f4e5ae5fd0b1f
Ahsan <ahsanfarooq210@gmail.com>
2020-07-07 09:49:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/search_worker/Activity/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.search_worker.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ScrollView;
+
+import com.example.search_worker.R;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ScrollView scrollView;
+    private Handler handler;
+    Runnable runnable=new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            scrollView.setVisibility(View.VISIBLE);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initializing the scroll view
+        scrollView=findViewById(R.id.dashboard_scroll_view);
+
+        //initializing the handler for the spalsh screen
+        handler=new Handler();
+
+        //splash screen
+        handler.postDelayed(runnable,1000);//1 second is the timeout for the splash screen
+
+
+
+    }
+
+    public void searchResume(View view)
+    {
+    }
+
+    public void editResume(View view)
+    {
+    }
+
+    public void addResume(View view)
+    {
+    }
+}
---
+++ app/src/main/java/com/example/search_worker/Entity/ModelClass.java
@@ -0,0 +1,5 @@
+package com.example.search_worker.Entity;
+
+public class ModelClass
+{
+}
