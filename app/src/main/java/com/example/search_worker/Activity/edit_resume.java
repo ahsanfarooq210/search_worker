681ad0c9bc7354b4c164cca720b27c4ced455ce9
Ahsan <ahsanfarooq210@gmail.com>
2020-07-07 12:39:41 +0500

    search resume

--- app/src/main/java/com/example/search_worker/Activity/edit_resume.java
+++ app/src/main/java/com/example/search_worker/Activity/edit_resume.java
@@ -3,16 +3,28 @@ package com.example.search_worker.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.search_worker.R;
 
+import java.security.PrivateKey;
+
 public class edit_resume extends AppCompatActivity
 {
+    private EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_resume);
+
+        username=findViewById(R.id.username_tf);
+        password=findViewById(R.id.password_tf);
+    }
+
+    public void loginButton(View view)
+    {
     }
 }
