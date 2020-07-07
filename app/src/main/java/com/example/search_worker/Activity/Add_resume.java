b4dbf3fc8c29d48394495fcd4c806cb6f80b2ed4
Ahsan <ahsanfarooq210@gmail.com>
2020-07-07 10:28:41 +0500

    initial commit

--- app/src/main/java/com/example/search_worker/Activity/Add_resume.java
+++ app/src/main/java/com/example/search_worker/Activity/Add_resume.java
@@ -3,6 +3,7 @@ package com.example.search_worker.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.search_worker.R;
 
@@ -14,5 +15,23 @@ public class Add_resume extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_resume);
+    }
+
+    public void create(View view)
+    {
+    }
+
+    public void addResearchButton(View view)
+    {
+    }
+
+
+
+    public void addProject(View view)
+    {
+    }
+
+    public void addEducation(View view)
+    {
     }
 }
